0f5176d237f6fd13b918b8e48b3e979e77cb0afd
Paul Yaro <alassane-paul.yaro@outlook.fr>
2025-07-07 18:10:53 +0200

    implement gpt-4.1-nano DataFrame query using e-commerce-dataset.csv

--- langchainFuncs.py
+++ langchainFuncs.py
@@ -2,7 +2,12 @@ import streamlit as st
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import SystemMessage, AIMessage, HumanMessage
 
-chain = ChatOpenAI(model="gpt-4o", api_key=st.secrets["OPENAI_API_KEY"])
+from langchain.prompts import PromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import RunnableBranch, RunnableLambda, RunnablePassthrough
+import pandas as pd
+
+""" chain = ChatOpenAI(model="gpt-4o", api_key=st.secrets["OPENAI_API_KEY"])
 
 revenueData = '''
 January: $13,250
@@ -26,4 +31,25 @@ messages = [
 
 response = chain.invoke(messages)
 print("Full LLM Response: ", response)
-print("LLM Response Content: ", response.content)
+print("LLM Response Content: ", response.content) """
+
+df =  pd.read_csv("e-commerce-dataset.csv")
+print(df.head())
+
+llm = ChatOpenAI(model="gpt-4.1-nano", api_key=st.secrets["OPENAI_API_KEY"])
+
+# prompts
+retrieval_prompt = PromptTemplate.from_template(
+    '''Based on the following input: {input_text}, create the appropriate query to be used to filter and return results from a python dataframe (df.query()).
+    The dataframe has column values {col_vals}. Only return the query to be used inside the df.query function and nothing else'''
+)
+
+retrieval_prompt = retrieval_prompt.partial(col_vals = df.columns)
+
+# chain
+chain = retrieval_prompt | llm | StrOutputParser()
+
+# run
+user_input = {"input_text": "Please give me all the sales records for Wireless Mouse"}
+response = chain.invoke(user_input)
+print(response)
